164b0c81502cb1dd90e73d1edcf825cd0162630d
anushka <anushkashrestha24@gmail.com>
2018-12-07 17:57:21 +0545

    Final Design

---
+++ scripts/hamburger.js
@@ -0,0 +1,7 @@
+function onClickFunction() {
+  if (document.getElementsByClassName("ham")[0].style.display == 'none') {
+    document.getElementsByClassName("ham")[0].style.display = 'block';
+  } else {
+    document.getElementsByClassName("ham")[0].style.display = 'none';
+  }
+ }
